4096e4a0112bfa0b101a48f6b0b213b1b4353e2c
karanmittal-darwix <karan.mittal@darwix.ai>
2026-05-06 13:22:26 +0530

    fix

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,11 +1,1 @@
 
-import VoicePlayground from "@/components/voice/VoicePlayground";
-
-export default function Home() {
-  return (
-    <main>
-      <VoicePlayground />
-      
-    </main>
-  );
-}
